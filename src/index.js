c7fa4f534dcbd1d8f52721165d9bcb4a3dfa6eae
Joe Schneider <schne152@mail.chapman.edu>
2023-03-05 17:59:11 -0700

    added firebase auth

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -28,6 +28,7 @@ initializeApp(firebaseConfig);
 const db = getFirestore()
 // collection ref
 const colRef = collection(db, 'movie')
+const auth = getAuth()
 // get colleciton data
 getDocs(colRef)
   .then((snapshot)=>{
